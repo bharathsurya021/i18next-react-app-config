285aa09e46512ba61c589135cf9f565af2e9c9e1
bharathsurya021 <bharathsurya021@gmail.com>
2023-01-09 17:09:15 +0530

    (feat) language select

--- src/App.js
+++ src/App.js
@@ -4,24 +4,35 @@ import './App.css';
 import { useState } from 'react';
 import { withTranslation } from 'react-i18next'
 
-const lngs = {
-  en: { nativeName: 'English' },
-  de: { nativeName: 'Deutsch' }
-}
-
 function App({ t, i18n }) {
   const [count, setCounter] = useState(0);
+  const handleChangeLanguage = (e) => {
+
+    i18n.changeLanguage(e.target.value)
+    sessionStorage.setItem("lang", e.target.value)
+    setCounter(count + 1)
+
+
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
-          {Object.keys(lngs).map((lng) => {
+          {/* {Object.keys(lngs).map((lng) => {
             return (<button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => { i18n.changeLanguage(lng); setCounter(count + 1) }}> {lngs[lng].nativeName}
 
             </button>)
-          })}
+          })} */}
+
+          <select
+            defaultValue={sessionStorage.getItem("lang")}
+            onChange={handleChangeLanguage}>
+            <option value="en">English</option>
+            <option value="de">Deustch</option>
+          </select>
         </div>
+
         <p>
           <i>{t('counter', { count })}</i>
         </p>
